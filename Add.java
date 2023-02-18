129c0b7a1924ff7f9fb71a357a0824f54cac2042
ASHWINI007TYAGI <125735781+ASHWINI007TYAGI@users.noreply.github.com>
2023-02-18 16:22:24 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add
+{
+    public static void main(String[]argh)
+    {
+        int a=8;
+        int b=2;
+        int c =a+b;
+        System.out.println("THE SUM OF A AND B IS = "+c);
+    }
+}
